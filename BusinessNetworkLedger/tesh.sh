1f7924dbc15afb39b96a7f6351276339a17a7843
greenshrek <pranavsrivastava108@gmail.com>
2019-04-14 11:31:39 +0200

    updated

---
+++ BusinessNetworkLedger/tesh.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#test AlertingSystem
+
+python kpn-technium/AlertingSystem/graph.py
+
+python kpn-technium/AlertingSystem/tempCodeRunnerFile.py
+
+
+#test BarcodeAndQRReaderAndroid-master
+java kpn-technium/BarcodeAndQRReaderAndroid-master/app/src/main/java/com/nextinnovation/pt/barcodescanner/activity/MainActivity.java
+
+
+#test Business Network Ledger
+kpn-technium/BusinessNetworkLedger/eggnet/lib/script.js
+
+kpn-technium/BusinessNetworkLedger/eggnet/models/sample.cto
+
+#run the postman collection
+kpn-technium/BusinessNetworkLedger/test/PostmanCollection.json
